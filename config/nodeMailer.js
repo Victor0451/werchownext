2c14b4e98b249bc6c8d2dc7369c6ea087d1fa8fb
Victor Longo <victor.m.longo@gmail.com>
2021-11-29 20:32:14 -0300

    correccion de errores.

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,4 +1,5 @@
-import nodemailer from "nodemailer";
+
+const nodemailer = require('nodemailer')
 
 const username = process.env.NODEMAILER_USERNAME;
 const password = process.env.NODEMAILER_PASSWORD;
