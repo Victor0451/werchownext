b35184ad33a8f3a6066c099bf520913f21eda63a
Victor Longo <victor.m.longo@gmail.com>
2023-05-24 11:50:17 -0300

    Aumento en tasa de prestamos

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 // INTERES PRESTAMOS
-export const tp3 = 190 / 100 / 12;
-export const tp6 = 200 / 100 / 12;
-export const tp10 = 210 / 100 / 12;
-export const tp12 = 220 / 100 / 12;
+export const tp3 = 210/ 100 / 12;
+export const tp6 = 220 / 100 / 12;
+export const tp10 = 230 / 100 / 12;
+export const tp12 = 240 / 100 / 12;
 export const tpemp = 100 / 100 / 12;
