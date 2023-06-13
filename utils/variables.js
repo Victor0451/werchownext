62adaee012284363cc6f6b8f02bef03a52ffb7ca
Victor Longo <victor.m.longo@gmail.com>
2023-06-13 18:47:06 -0300

    Suba de un 5% en la tasa de los prestamos a policias

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 // INTERES PRESTAMOS
-export const tp3 = 210/ 100 / 12;
-export const tp6 = 220 / 100 / 12;
-export const tp10 = 230 / 100 / 12;
-export const tp12 = 240 / 100 / 12;
+export const tp3 = 215/ 100 / 12;
+export const tp6 = 225 / 100 / 12;
+export const tp10 = 235 / 100 / 12;
+export const tp12 = 245 / 100 / 12;
 export const tpemp = 100 / 100 / 12;
