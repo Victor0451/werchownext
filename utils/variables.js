b09f347a69e9e4ee85eb9011466bcf861731ecd4
Longo Victor <victor.m.longo@gmail.com>
2023-08-17 11:34:35 -0300

    Actualizacion de tasas en prestamos a policias y empleados

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 // INTERES PRESTAMOS
-export const tp3 = 215/ 100 / 12;
-export const tp6 = 225 / 100 / 12;
-export const tp10 = 235 / 100 / 12;
-export const tp12 = 245 / 100 / 12;
-export const tpemp = 100 / 100 / 12;
+export const tp3 = 240/ 100 / 12;
+export const tp6 = 250 / 100 / 12;
+export const tp10 = 260 / 100 / 12;
+export const tp12 = 270 / 100 / 12;
+export const tpemp = 125 / 100 / 12;
