2449e798d610b8f5e7b8f2762e4992b603148c2b
Longo Victor <victor.m.longo@gmail.com>
2023-10-11 10:53:37 -0300

    Aumento de tasa de prestamos en 10 puntos c/u

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,6 @@
 // INTERES PRESTAMOS
-export const tp3 = 255/ 100 / 12;
-export const tp6 = 265 / 100 / 12;
-export const tp10 = 275 / 100 / 12;
-export const tp12 = 285 / 100 / 12;
-export const tpemp = 140 / 100 / 12;
+export const tp3 = 265 / 100 / 12;
+export const tp6 = 275 / 100 / 12;
+export const tp10 = 285 / 100 / 12;
+export const tp12 = 295 / 100 / 12;
+export const tpemp = 150 / 100 / 12;
