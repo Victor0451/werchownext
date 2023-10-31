6920e1656ac8f732a70a43d530a615c56d9de12e
Longo Victor <victor.m.longo@gmail.com>
2023-10-31 10:40:50 -0300

    Incorporacion de aviso sobre listado de prestamos pendientes por mail

--- pages/home/home.js
+++ pages/home/home.js
@@ -13,6 +13,8 @@ import Router from "next/router";
 import AccesosRapidos from "../../components/home/AccesosRapidos";
 import ModalNovedades from "../../components/layout/ModalNovedades";
 import Notificaciones from "../../components/home/Notificaciones";
+import moment from "moment";
+import toastr from "toastr";
 
 const STATE_INICIAL = {
   socio: "",
@@ -21,33 +23,6 @@ const STATE_INICIAL = {
 };
 
 const home = () => {
-  let token = jsCookie.get("token");
-
-  useEffect(() => {
-    if (!token) {
-      Router.push("/redirect");
-    } else {
-      let usuario = jsCookie.get("usuario");
-
-      if (usuario) {
-        let userData = JSON.parse(usuario);
-        guardarUsuario(userData.perfil);
-
-        traerMensajes(userData.usuario)
-      }
-
-      traerNovs()
-
-      ordenesPendientes()
-
-      prestamosPendientes()
-
-
-    }
-  }, []);
-
-
-
   const [error, guardarError] = useState(false);
   const [novedades, guardarNovedades] = useState(null);
   const [socioRes, guardarSocio] = useState(null);
@@ -57,8 +32,6 @@ const home = () => {
   const [msj, guardarMensajes] = useState(0);
   const [prest, guardarPrest] = useState(0);
   const [orde, guardarOrde] = useState(0);
-
-
 
   const { valores, errores, handleChange, handleSubmit, handleBlur } =
     useValidacion(STATE_INICIAL, validarBuscarSocio, buscarSocio);
@@ -135,56 +108,64 @@ const home = () => {
   }
 
   const ShowModalNovs = () => {
-    document.getElementById('showModal').click()
-
-  }
+    document.getElementById("showModal").click();
+  };
 
   const traerNovs = async () => {
-
-    await axios.get(`${ip}api/sgi/novedades/traernovedades`)
-      .then(res => {
-
+    await axios
+      .get(`${ip}api/sgi/novedades/traernovedades`)
+      .then((res) => {
         if (res.status === 200) {
-          guardarNovedades(res.data)
-          ShowModalNovs()
+          guardarNovedades(res.data);
+          ShowModalNovs();
         }
       })
-      .catch(error => {
-        console.log(error)
-      })
-
-  }
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   const traerMensajes = async (id) => {
-
-    await axios.get(`${ip}api/sgi/mails/listmsjsinleer/${id}`)
-      .then(res => {
-
+    await axios
+      .get(`${ip}api/sgi/mails/listmsjsinleer/${id}`)
+      .then((res) => {
         if (res.status === 200) {
-
-          guardarMensajes(res.data.length)
-
+          guardarMensajes(res.data.length);
         }
-
       })
-      .catch(error => {
+      .catch((error) => {
+        console.log(error);
+        toastr.error("Ocurrio un error al traer los mensajes", "ATENCION");
+      });
+  };
 
-        console.log(error)
-        toastr.error("Ocurrio un error al traer los mensajes", "ATENCION")
-
-      })
-
-  }
-
-  const prestamosPendientes = async () => {
-
+  const prestamosPendientes = async (per) => {
     await axios
       .get(`${ip}api/sgi/prestamos/listadoprestamospendientes`)
       .then((res) => {
         if (res.data.length !== 0) {
-
           guardarPrest(res.data.length);
 
+          parImpar(res.data, per);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const mandarMail = (array) => {
+    fetch("/api/mail/sgi/prestamos", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(array),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          jsCookie.set("env", true);
         }
       })
       .catch((error) => {
@@ -193,34 +174,59 @@ const home = () => {
   };
 
   const ordenesPendientes = async () => {
-
-    await axios.get(`${ip}api/sgi/ordenpago/traerordenespendientes`)
-      .then(res => {
-
-        guardarOrde(res.data.length)
-
+    await axios
+      .get(`${ip}api/sgi/ordenpago/traerordenespendientes`)
+      .then((res) => {
+        guardarOrde(res.data.length);
       })
-      .catch(error => {
-        console.log(error)
-        toastr.error("Ocurrio un error al generar el listado", "ATENCION")
+      .catch((error) => {
+        console.log(error);
+        toastr.error("Ocurrio un error al generar el listado", "ATENCION");
+      });
+  };
 
-      })
+  const parImpar = (arr, per) => {
+    let numero = moment().format("DD");
+    let f = jsCookie.get("env");
+    if (per === 1 || per === 3) {
+      if (f !== "true") {
+        if (numero % 2 == 0 && arr.length > 0) {
+          mandarMail(arr);
+        }
+      }
+    }
+  };
 
-  }
+  let token = jsCookie.get("token");
 
+  useEffect(() => {
+    if (!token) {
+      Router.push("/redirect");
+    } else {
+      let usuario = jsCookie.get("usuario");
+
+      if (usuario) {
+        let userData = JSON.parse(usuario);
+        guardarUsuario(userData.perfil);
+
+        traerMensajes(userData.usuario);
+
+        prestamosPendientes(userData.perfil);
+      }
+
+      traerNovs();
+
+      ordenesPendientes();
+    }
+  }, []);
 
   return (
     <Layout>
       <div>
-
         <Noticias user={user} />
 
         {user == 1 || user == 3 ? (
-          <Notificaciones
-            msj={msj}
-            prest={prest}
-            orde={orde}
-          />
+          <Notificaciones msj={msj} prest={prest} orde={orde} />
         ) : null}
 
         <AccesosRapidos user={user} />
@@ -240,18 +246,20 @@ const home = () => {
             listSocio={listSocio}
           />
         ) : null}
-
-
       </div>
-
 
       <ModalNovedades novedades={novedades} />
 
-
-      <button hidden type="button" id="showModal" className="btn btn-primary" data-toggle="modal" data-target="#novedades">
+      <button
+        hidden
+        type="button"
+        id="showModal"
+        className="btn btn-primary"
+        data-toggle="modal"
+        data-target="#novedades"
+      >
         Launch demo modal
       </button>
-
     </Layout>
   );
 };
